255fa822c8cf1dbd2b2e98f1ad56960bce9651c5
Damyan Peykov <dppeykov@gmail.com>
2021-01-03 16:05:47 +0100

    adding the first data as a dictionary and using the JsonResponce to serialize it

--- firstApp/views.py
+++ firstApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+# Funcion-based view
+
+
+def employeeView(request):
+    emp = {'id': 123, 'name': 'John', 'salary': 1000000}
+    return JsonResponse(emp)
